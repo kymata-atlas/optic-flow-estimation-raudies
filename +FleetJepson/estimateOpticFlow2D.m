987f5974f1999a0cda3c8a85de6fd80683e287ee
Cai Wingfield <cai@caiwingfield.net>
2023-06-15 08:34:20 +0100

    Consistent coding style

--- +FleetJepson/estimateOpticFlow2D.m
+++ +FleetJepson/estimateOpticFlow2D.m
@@ -1,10 +1,10 @@
-function [Dx Dy] = estimateOpticFlow2D(ImgSeq, opt)
-% estimateOpticFlow2D
-%   ImgSeq  - Image sequence as a cube with dimensions: 
+function [Dx, Dy] = estimateOpticFlow2D(ImgSeq, opt)
+% estimateOpticFlow2D (Fleet & Jepson)
+%   ImgSeq  - Image sequence as a cube with dimensions:
 %             height x width x frames.
 %   opt     - Struture with options:
 %             * f       - Spatio-temporal frequency in cycles per pixel or
-%                         cycles per frame. The spatial frequency f is the 
+%                         cycles per frame. The spatial frequency f is the
 %                         radius of ceil(pi/2*(2^beta+1)/(2^beta-1)) tested
 %                         orientations.
 %             * beta    - Frequency bandwidth in octaves.
@@ -20,9 +20,9 @@ function [Dx Dy] = estimateOpticFlow2D(ImgSeq, opt)
 %             and Dy are of dimensions: height x width.
 %
 % DESCRIPTION
-%   An implementation of the idea of 
+%   An implementation of the idea of
 %   Fleet, D. and Jepson, A.D. (1990). Computation of component image
-%       velocity from local phase information. International Journal of 
+%       velocity from local phase information. International Journal of
 %       Computer Vision 5(1), 77-104.
 %   Note that the paper describes most parameters of the algorithm on p. 86
 %   as speed values, number of directions, and directional increment.
@@ -30,26 +30,28 @@ function [Dx Dy] = estimateOpticFlow2D(ImgSeq, opt)
 %   Gabor filters. For instance, using ds = pi/sigma is not defined for
 %   zero speeds.
 %   A shorter and sometimes clearer write-up of the method is given in
-%   Fleet, D.J. and Jepson, A.D. (1989). Computation of normal velocity 
-%       from local phase information. In Proceedings of Computer Vision and 
+%   Fleet, D.J. and Jepson, A.D. (1989). Computation of normal velocity
+%       from local phase information. In Proceedings of Computer Vision and
 %       Pattern Recognition, CVPR'89. 379-386.
-%   These papers do not describe how to combine responses from different 
-%   frequencies and in particular the normalization accross them. Here, I 
-%   weigh estimates by their amplitude, which is later re-normlized to get 
+%   These papers do not describe how to combine responses from different
+%   frequencies and in particular the normalization accross them. Here, I
+%   weigh estimates by their amplitude, which is later re-normlized to get
 %   the correct speed (see lines 108 and 131 & 132).
 %
 %   Copyright (C) 2013  Florian Raudies, 05/16/2013, Boston University.
 %   License, GNU GPL, free software, without any warranty.
 
 % Set default values for paraemters of the method.
-if nargin<2,                opt         = struct();         end % UNITS
-if ~isfield(opt,'f'),       opt.f       = 1/4;              end % cycles per pixel or frame
-if ~isfield(opt,'beta'),    opt.beta    = 0.8;              end % octaves
-if ~isfield(opt,'FreqTmp'), opt.FreqTmp = [-opt.f 0 opt.f]; end % temporal frequencies
+if nargin < 2,              opt         = struct();           end % UNITS
+if ~isfield(opt,'f'),       opt.f       = 1/4;                end % cycles per pixel or frame
+if ~isfield(opt,'beta'),    opt.beta    = 0.8;                end % octaves
+if ~isfield(opt,'FreqTmp'), opt.FreqTmp = [-opt.f, 0, opt.f]; end % temporal frequencies
+
 % Retrieve parameters.
 f           = opt.f;
 beta        = opt.beta;
 FreqTmp     = opt.FreqTmp;
+
 % Define additional prameters and Gabor functions.
 sigmaFreq   = f*(2^beta-1)/(2^beta+1);  % sigma in Fourier domain
 sigma       = 1/(2*pi*sigmaFreq);       % sigma in non-transformed domain
@@ -65,48 +67,51 @@ Kernel      = fKernel(sigma);
 kNum        = length(Kernel);
 yNum        = size(ImgSeq,1);
 xNum        = size(ImgSeq,2);
-nTmp        = length(FreqTmp);  % number of temporal frequencies
-D           = [-1 0 1]/2;       % differential operator
+nTmp        = length(FreqTmp);    % number of temporal frequencies
+D           = [-1, 0, 1]/2;       % differential operator
 dNum        = length(D);
-% Define the space for angular frequencies in x, y, and t. These are named 
+
+% Define the space for angular frequencies in x, y, and t. These are named
 % kx, ky, and omega, respectively.
 Kx      = zeros(yNum,xNum,nOri,nTmp); % spatial frequency
 Ky      = zeros(yNum,xNum,nOri,nTmp); % spatial frequency
 Omega   = zeros(yNum,xNum,nOri,nTmp); % temporal frequency
 A       = zeros(yNum,xNum,nOri,nTmp); % amplitude signal
+
 % Check if the provided sequence contains enough frames.
 tNum    = size(ImgSeq,3);
-if tNum<(kNum+2), 
+if tNum<(kNum+2),
     error('MATLAB:frameErr', ['This method requires at least %d frames ',...
         'but only %d frames were provided!'], kNum+2, tNum);
-end
+end % if tNum
 % Select the center number of kNum+2 frames.
 center  = ceil(tNum/2);
 SelT    = center + ( -(kNum-1)/2-1 : +(kNum-1)/2+1 ); % +-1 for D.
 ImgSeq  = ImgSeq(:,:,SelT);
+
 % Compute the angular frequencies in spatial and temporal domain.
 for iTmp = 1:nTmp,
     ft = FreqTmp(iTmp);
-    Gabor.SinT = reshape(fGaborSin(Kernel,ft,sigma),   [1 1 kNum]);
-    Gabor.CosT = reshape(fGaborCos(Kernel,ft,sigma,b), [1 1 kNum]);
+    Gabor.SinT = reshape(fGaborSin(Kernel,ft,sigma),   [1, 1, kNum]);
+    Gabor.CosT = reshape(fGaborCos(Kernel,ft,sigma,b), [1, 1, kNum]);
     for iOri = 1:nOri,
         fx = f*cos(Ori(iOri));
         fy = f*sin(Ori(iOri));
-        Gabor.SinX = reshape(fGaborSin(Kernel,fx,sigma),   [1 kNum 1]);
-        Gabor.CosX = reshape(fGaborCos(Kernel,fx,sigma,b), [1 kNum 1]);
-        Gabor.SinY = reshape(fGaborSin(Kernel,fy,sigma),   [kNum 1 1]);
-        Gabor.CosY = reshape(fGaborCos(Kernel,fy,sigma,b), [kNum 1 1]);
+        Gabor.SinX = reshape(fGaborSin(Kernel,fx,sigma),   [1, kNum, 1]);
+        Gabor.CosX = reshape(fGaborCos(Kernel,fx,sigma,b), [1, kNum, 1]);
+        Gabor.SinY = reshape(fGaborSin(Kernel,fy,sigma),   [kNum, 1, 1]);
+        Gabor.CosY = reshape(fGaborCos(Kernel,fy,sigma,b), [kNum, 1, 1]);
         % Compute the Gabor filter response.
         R   = filterSepGabor(ImgSeq, Gabor);
         % Compute the temoral derivative.
-        Rt  = convn(R, reshape(D, [1 1 dNum]), 'valid');
+        Rt  = convn(R, reshape(D, [1, 1, dNum]), 'valid');
         % Select the center frame.
         R   = squeeze(R(:,:, 2));
         % Compute the spatial derivatives.
-        Rx  = imfilter(R, reshape(D, [1 dNum 1]), 'same', 'replicate');
-        Ry  = imfilter(R, reshape(D, [dNum 1 1]), 'same', 'replicate');
+        Rx  = imfilter(R, reshape(D, [1, dNum, 1]), 'same', 'replicate');
+        Ry  = imfilter(R, reshape(D, [dNum, 1, 1]), 'same', 'replicate');
         RInv = 1./(abs(R) + eps);
-        % Compute the spatial and temporal phases and add them to the 
+        % Compute the spatial and temporal phases and add them to the
         % tested ones. See Fleet & Jepson (1989), Eq. (6) on page 380.
         Kx(:,:,iOri,iTmp)    = (imag(Rx).*real(R) ...
                                -real(Rx).*imag(R)).*RInv + 2*pi*fx;
@@ -115,21 +120,27 @@ for iTmp = 1:nTmp,
         Omega(:,:,iOri,iTmp) = (imag(Rt).*real(R) ...
                                -real(Rt).*imag(R)).*RInv + 2*pi*ft;
         A(:,:,iOri,iTmp)     = abs(R);
-    end
-end
+    end % for iOri
+end % for iTmp
+
 % Put all constraints into a single dimension.
-Kx      = reshape(Kx,    [yNum xNum nOri*nTmp]);
-Ky      = reshape(Ky,    [yNum xNum nOri*nTmp]);
-Omega   = reshape(Omega, [yNum xNum nOri*nTmp]);
-A       = reshape(A,     [yNum xNum nOri*nTmp]);
+Kx      = reshape(Kx,    [yNum, xNum, nOri*nTmp]);
+Ky      = reshape(Ky,    [yNum, xNum, nOri*nTmp]);
+Omega   = reshape(Omega, [yNum, xNum, nOri*nTmp]);
+A       = reshape(A,     [yNum, xNum, nOri*nTmp]);
+
 % Compute the normal vector.
 LenInv  = 1./(hypot(Kx, Ky) + eps);
 Nx      = +Kx.*LenInv;      % x-component of normal
 Ny      = +Ky.*LenInv;      % y-component of normal
 S       = -Omega.*LenInv;   % speed along normal
+
 % Compute normal flow. Noramlization using the amplitude.
 Dx      = squeeze(mean(S.*Nx,3)./mean(A,3));
 Dy      = squeeze(mean(S.*Ny,3)./mean(A,3));
+
+end %function estimateOpticFlow2D
+
 
 
 function ImgSeq = filterSepGabor(ImgSeq, Gabor)
@@ -149,3 +160,5 @@ ImgSeq  = imfilter(GaborCosTCosX, Gabor.CosY, 'same', 'replicate') ...
         - imfilter(GaborSinTSinX, Gabor.SinY, 'same', 'replicate') ...
         + imfilter(GaborCosTSinX, Gabor.CosY, 'same', 'replicate') ...
         + imfilter(GaborCosTCosX, Gabor.SinY, 'same', 'replicate'));
+end %function filterSepGabor
+
